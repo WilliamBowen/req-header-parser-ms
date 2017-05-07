4a81813fd7e80daae16e29381ab7c02859f992b5
William Bowen <bowenw@onid.oregonstate.edu>
2017-05-07 08:17:16 +0000

    app returns ip address, language, and browser information

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ var express = require("express")
 var app = express()
 
 app.get('/', function(req, res) {
-  res.send("Hello World!")
+  var ip = req.headers['x-forwarded-for']
+  var language = req.headers['accept-language'].split(',')[0]
+  var software = req.headers['user-agent'].match(/\(([^)]+)\)/)[1]
+
+  var identity = {
+    'ipaddress' : ip,
+    'language' : language,
+    'software' : software
+  }
+  res.send(JSON.stringify(identity))
 })
 app.listen(8080, function() {
   console.log("app listening on port 8080")
